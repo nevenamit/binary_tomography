c8b610e14e3a93e2a34e1dd15729fd58eb88dc5a
Viktor Zivkovic <vzivkovic26@gmail.com>
2025-07-17 10:26:31 +0200

    added gauss regularization from nagy's paper

--- cost_functions.py
+++ cost_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.ndimage as ndi
+import matplotlib.pyplot as plt
 
 def _total_variation(X):
     N = int(np.sqrt(X.shape[0]))
@@ -37,3 +38,62 @@ def l2_and_tv(X, W, sino_target, lambda_tv):
     tv = _total_variation(X)
 
     return l2 + lambda_tv * tv
+
+
+def _compute_spacial_lut(radius, sigma, print=False):
+    x = np.arange(-radius, radius+1)
+    X, Y = np.meshgrid(x, x)
+    lut = np.exp(-(X**2 + Y**2)/(2*sigma**2))
+
+    if print:
+        fig =  plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(X, Y, lut, cmap='coolwarm')
+
+    return lut
+
+def compute_regularization_term(
+    image : np.ndarray,
+    sigma : float, # variance of gaussian used
+    r     : int, # radius of window used, size of window is 2r+1 x 2r+1
+):
+    spacial_lut = _compute_spacial_lut(r, sigma)
+
+    N = image.shape[0]
+    if image.shape[1] != image.shape[0]:
+        raise ValueError("image should be square")
+        
+    cumulative_error = 0
+
+    for i in range(N):
+        for j in range(N):
+            left_ind = max(j - r, 0)
+            right_ind = min(j + r + 1, N)
+            up_ind = max(i - r, 0)
+            down_ind = min(i + r + 1, N)
+
+            left_lut = r - (j - left_ind)
+            right_lut = left_lut + (right_ind - left_ind)
+            up_lut = r - (i - up_ind)
+            down_lut = up_lut + (down_ind - up_ind) 
+
+            slice = image[up_ind:down_ind, left_ind:right_ind]
+            slice = np.abs(slice - image[i,j])
+
+            lut = spacial_lut[up_lut:down_lut, left_lut:right_lut]
+            
+            cumulative_error += np.sum(slice * lut)
+            
+    return cumulative_error
+
+def l2_gauss_regularization(
+    X : np.ndarray,
+    W : np.ndarray,
+    sino_target : np.ndarray,
+    sigma : float, # variance of gaussian used
+    r     : int, # radius of window used, size of window is 2r+1 x 2r+1
+):
+    l2 = l2_norm(X, W, sino_target)
+    gauss_reg = compute_regularization_term(X.reshape(int(np.sqrt(X.shape[0])), -1), sigma, r)
+
+    return l2 + gauss_reg
